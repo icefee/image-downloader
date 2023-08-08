bca99e59440e71b0d91d2369e28248077bdc43c3
ying_123 <zhangying@aten-optics.com>
2023-08-08 08:24:02 +0800

    update match rule

--- lib/tools/api.dart
+++ lib/tools/api.dart
@@ -4,20 +4,21 @@ import 'package:http/http.dart';
 Future<List<String>> getImages(String url) async {
   Response response = await get(Uri.parse(url));
   Iterable<RegExpMatch> matches =
-      RegExp(r'(https?://)?[\w\u4e00-\u9fa5-./@%?:=]+?\.((jpe?|pn)g|gif|webp)', caseSensitive: false)
+      RegExp(r'(https?://)?[\w\u4e00-\u9fa5-./@%?:]+?\.((jpe?|pn)g|gif|webp)', caseSensitive: false)
           .allMatches(response.body.replaceAll(RegExp(r'\\/'), '/'));
   List<String> images = matches.map((m) => m.group(0)!).toSet().toList();
   Uri uri = Uri.parse(url);
   for (int i = 0; i < images.length; i++) {
-    if (!images[i].startsWith(RegExp(r'https?://', caseSensitive: false))) {
-      if (images[i].startsWith('/')) {
-        if (images[i].startsWith('//')) {
-          images[i] = '${uri.scheme}:${images[i]}';
+    String image = images[i];
+    if (!image.startsWith(RegExp(r'https?://', caseSensitive: false))) {
+      if (image.startsWith('/')) {
+        if (image.startsWith('//')) {
+          images[i] = '${uri.scheme}:$image';
         } else {
-          images[i] = uri.origin + images[i];
+          images[i] = uri.origin + image;
         }
       } else {
-        images[i] = uri.origin + (uri.path.isNotEmpty ? uri.path : '/') + images[i];
+        images[i] = uri.origin + (uri.path.isNotEmpty ? uri.path : '/') + image;
       }
     }
   }
